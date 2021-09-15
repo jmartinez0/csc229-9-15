66df86f0ba7c77c4916b3dc14e60d4d5c56da274
juan <juan@localhost>
2021-09-15 13:01:36 -0400

    First commit

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,21 @@
+//@author juan
+
+public class NewClass {
+    int getLargest(int[] nmList ) {
+        int max = nmList [0];
+        int indx = 0;
+        while( indx < nmList.length ) {
+            if (nmList [indx] > max) {
+                    max = nmList [indx];
+              }
+            indx++;
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        NewClass nc = new NewClass();
+        int[] arr = { 3, 6, -1, 10, 15, -8};
+        int mx = nc.getLargest(arr);
+        System.out.println("The max is "+ mx);
+    }
+}
